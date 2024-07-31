6362644ef05cc27bc315e62d1056582a1370958d
USER <user@MacPro14-M2-Pro---AL02416135.local>
2024-07-31 18:24:12 +0900

    Update to V3

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# gulp-ejs-scss-sprite-browsersync
-gulp ejs scss sprite browsersync template
+# gulp-ejs-scss-browsersync
+
+gulp ejs scss browsersync template
 
 ```sh
 npm i -g gulp-cli
@@ -7,13 +8,14 @@ npm i
 ```
 
 ### tasks
-- `npm start`: ejs,scss,sprite + browsersync server
+
+- `npm start`: ejs,scss + browsersync server
 - `npm run deploy`: deploy gh-pages branch
 
 ### packages
+
 - [gulp](https://gulpjs.com/)
 - [gulp-ejs](https://github.com/rogeriopvl/gulp-ejs)
   - [https://ejs.co/](https://ejs.co/)
 - [gulp-sass](https://github.com/dlmanning/gulp-sass)
 - [browsersync](https://browsersync.io/)
-- [gulp.spritesmith](https://github.com/twolfson/gulp.spritesmith)
--- gulpfile.js
+++ gulpfile.js
@@ -1,121 +1,122 @@
-const { src, dest, series, parallel, watch } = require("gulp");
-const ejs = require("gulp-ejs");
-const rename = require("gulp-rename");
-const del = require("del");
-const sass = require("gulp-sass");
-const sourcemaps = require("gulp-sourcemaps");
-const spritesmith = require("gulp.spritesmith");
-const merge = require("merge-stream");
-const browserSync = require("browser-sync").create();
-const generateIndxing = require("generate-index");
-const minifyHtml = require("gulp-html-minifier");
-const prettyHtml = require("gulp-pretty-html");
-const autoprefixer = require("gulp-autoprefixer");
-const babel = require("gulp-babel");
+import { src, dest, series, parallel, watch } from "gulp";
+import ejs from "gulp-ejs";
+import rename from "gulp-rename";
+import { deleteAsync } from "del";
+import sourcemaps from "gulp-sourcemaps";
+import browserSyncModule from "browser-sync";
+import generateIndxing from "generate-index";
+import minifyHtml from "gulp-html-minifier";
+import prettyHtml from "gulp-pretty-html";
+import autoprefixer from "gulp-autoprefixer";
+import babel from "gulp-babel";
+import * as dartSass from "sass";
+import gulpSass from "gulp-sass";
+const sass = gulpSass(dartSass);
+const browserSync = browserSyncModule.create();
 
 const PATH = {
   ejs: "src/views",
   scss: "src/scss",
   js: "src/js",
   assets: "src/assets",
-  sprites: "src/sprites",
-  sprite_scss: "src/scss/common",
-  sprite_image: "src/assets/images",
-  dist: "dist"
+  dist: "dist",
 };
 
-const html = () => src(`${PATH.ejs}/*.ejs`)
-  .pipe(ejs())
-  .pipe(rename({ extname: ".html" }))
-  .pipe(dest(PATH.dist));
+const html = () =>
+  src(`${PATH.ejs}/*.ejs`)
+    .pipe(ejs())
+    .pipe(rename({ extname: ".html" }))
+    .pipe(dest(PATH.dist));
 
-const css = () => src(`${PATH.scss}/*.scss`)
-  .pipe(sourcemaps.init())
-  .pipe(sass({
-    errLogToConsole: true,
-    outputStyle: "compact",
-    onError: browserSync.notify
-  }))
-  .pipe(sourcemaps.write("./maps"))
-  .pipe(dest(`${PATH.dist}/css`))
-  .pipe(browserSync.stream());
+const css = () =>
+  src(`${PATH.scss}/*.scss`)
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        errLogToConsole: true,
+        outputStyle: "expanded",
+        onError: browserSync.notify,
+      })
+    )
+    .pipe(sourcemaps.write("./maps"))
+    .pipe(dest(`${PATH.dist}/css`))
+    .pipe(browserSync.stream());
 
-const js = _ => src(`${PATH.js}/*.js`)
-  .pipe(dest(`${PATH.dist}/js`))
-  .pipe(browserSync.stream());;
+const js = (_) =>
+  src(`${PATH.js}/**/*.js`)
+    .pipe(dest(`${PATH.dist}/js`))
+    .pipe(browserSync.stream());
 
-const assets = _ => src(`${PATH.assets}/**/*`)
-  .pipe(dest(`${PATH.dist}/assets`));
+const assets = (_) =>
+  src(`${PATH.assets}/**/*`, {
+    buffer: true,
+    removeBOM: false,
+  }).pipe(dest(`${PATH.dist}/assets`));
 
-const clean = () => del(`${PATH.dist}/**`);
+const clean = () => deleteAsync(`${PATH.dist}/**`);
 
-const sprite = _ => {
-  const spriteData = src(`${PATH.sprites}/*.png`)
-    .pipe(spritesmith({
-      imgName: "sprite.png",
-      padding: 2,
-      cssName: "_sprite.scss",
-      imgPath: `images/sprite.png`
-    }));
-  
-  const imgStream = spriteData.img.pipe(dest(PATH.sprite_image));
-  const cssStream = spriteData.css.pipe(dest(PATH.sprite_scss));
-  return merge(imgStream, cssStream);
-}
-
-const serve =  () => {
+const serve = () => {
   browserSync.init({
     server: {
       baseDir: PATH.dist,
       directory: true,
     },
     open: false,
-  })
+  });
 
   watch(`${PATH.ejs}/**/*.ejs`, html);
   watch(`${PATH.scss}/**/*.scss`, css);
-  watch(`${PATH.js}/*.js`, js);
+  watch(`${PATH.js}/**/*.js`, js);
   watch(`${PATH.assets}/**/*`, assets);
-  watch(`${PATH.sprites}/**/*`, sprite);
   watch(`${PATH.dist}/*.html`).on("change", browserSync.reload);
-}
+};
 
 const index = async () => {
   await generateIndxing({
-    projectName: "Enter a Project Name",
-    author: "Enter a user name",
+    projectName: "A Dot",
+    author: "c11g",
     srcDir: `${PATH.dist}/`,
     extention: ".html",
-    outFileName: "@index.html"
-  })
-}
+    outFileName: "@index.html",
+  });
+};
 
 const prettier = async () => {
   src(`${PATH.dist}/*.html`)
-  .pipe(await minifyHtml({
-    collapseWhitespace: true,
-    minifyCSS: true,
-  }))
-  .pipe(prettyHtml({
-    indent_size: 2,
-  }))
-  .pipe(dest(PATH.dist))
-}
+    .pipe(
+      await minifyHtml({
+        collapseWhitespace: true,
+        minifyCSS: true,
+      })
+    )
+    .pipe(
+      prettyHtml({
+        indent_size: 2,
+      })
+    )
+    .pipe(dest(PATH.dist));
+};
 
 const cssPrefixer = () =>
   src(`${PATH.dist}/css/*.css`)
-  .pipe(autoprefixer())
-  .pipe(dest(`${PATH.dist}/css`))
+    .pipe(autoprefixer())
+    .pipe(dest(`${PATH.dist}/css`));
 
 const jsBable = () =>
   src(`${PATH.dist}/js/script.js`)
-  .pipe(babel({
-    presets: ['@babel/preset-env']
-  }))
-  .pipe(dest(`${PATH.dist}/js`))
+    .pipe(
+      babel({
+        presets: ["@babel/preset-env"],
+      })
+    )
+    .pipe(dest(`${PATH.dist}/js`));
 
-  const dev = series(clean, sprite, parallel(html, css, js), assets);
-  const build = series(dev, parallel(cssPrefixer, jsBable), index, prettier);
-  
-  exports.build = build;
-  exports.default = series(dev, serve);
+const dev = series(clean, parallel(html, css, js), assets);
+
+export const build = series(
+  dev,
+  parallel(cssPrefixer, jsBable),
+  index,
+  prettier
+);
+export default series(dev, serve);
